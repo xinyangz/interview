2df3c2d16ac6b71029266d3d41c3d1a03a522e6e
t_103z <t_103z@yeah.net>
2016-10-19 23:33:29 +0800

    remove hard coded data from room initial state

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -15,30 +15,5 @@ export default {
       threeYear: 0
     }
   },
-  rooms: [
-    {
-      id: 0,
-      name: '计蒜课秋招（前端）',
-      logo: '',
-      candidates: [],
-      interviewer: 'Jasin Yip',
-      problems: []
-    },
-    {
-      id: 1,
-      name: '计蒜课秋招（后端）',
-      logo: '',
-      candidates: [
-        1,
-        2,
-        3
-      ],
-      interviewer: 'Jonh Ma',
-      problems: [
-        1,
-        2,
-        3
-      ]
-    }
-  ]
+  rooms: []
 };
