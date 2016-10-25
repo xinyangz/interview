b9ae653b5a962b2c2584ab547358907702778423
Fangke Ye <yefangke@yeah.net>
2016-10-26 00:01:41 +0800

    Reorganize file structure

--- api_server/interview/urls.py
+++ api_server/interview/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
-from . import views
+from . import user_views
 
 api_patterns = [
-    url(r'^user/login$', views.user_login, name='user-login'),
-    url(r'^user/logout$', views.user_logout, name='user-logout'),
-    url(r'^user/register$', views.user_register, name='user-register'),
+    url(r'^user/login$', user_views.user_login, name='user-login'),
+    url(r'^user/logout$', user_views.user_logout, name='user-logout'),
+    url(r'^user/register$', user_views.user_register, name='user-register'),
 ]
 
 urlpatterns = [
