0521aeef3807c32cf02f49d5852daea1b25d1b0f
xkt <xkt14@mails.tsinghua.edu.cn>
2016-11-14 16:11:09 +0800

    Delete debug part

--- src/actions/candidateManagerActions.js
+++ src/actions/candidateManagerActions.js
@@ -140,7 +140,6 @@ export function editCandidate(candidate) {
 }
 
 export function addCandidate(candidate) {
-  console.log(candidate);
   return dispatch => {
     dispatch(addCandidateBegin());
     return axios.post('/candidate?token=' + token, candidate)
