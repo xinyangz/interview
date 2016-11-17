153d612808513b6c8d561dbbf34700e466b475e2
wangp <wang-p14@mails.tsinghua.edu.cn>
2016-11-17 14:23:12 +0800

    Configure middleware for production

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,6 +1,8 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
+import {routerMiddleware} from 'react-router-redux';
+import {browserHistory} from 'react-router';
 
 export default function configureStore(initialState) {
   const middewares = [
@@ -9,6 +11,7 @@ export default function configureStore(initialState) {
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunkMiddleware,
+    routerMiddleware(browserHistory)
   ];
 
   return createStore(rootReducer, initialState, compose(
