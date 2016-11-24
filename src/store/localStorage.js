3400f8d212721751b173d00808ab8a875274b2b7
zhangxy <t_103z@yeah.net>
2016-11-24 19:35:20 +0800

    Add local storage timeout

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -1,6 +1,12 @@
 export const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state');
+    const serializedTime = localStorage.getItem('time');
+    const saveTime = JSON.parse(serializedTime);
+    // timeout 300
+    if (Date.now() - saveTime > 300 * 1000) {
+      return undefined;
+    }
     if (serializedState === null) {
       return undefined;
     }
@@ -13,7 +19,9 @@ export const loadState = () => {
 export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify(state);
+    const serializedTime = JSON.stringify(Date.now());
     localStorage.setItem('state', serializedState);
+    localStorage.setItem('time', serializedTime);
   } catch (error) {
     // ignore error
   }
